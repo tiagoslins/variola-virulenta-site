d7fcb520022315b1f38dd1b258e69510ed0905e4
tiago <tiagos.lins@hotmail.com>
2025-07-17 19:54:39 -0300

    FUll updade1.2.3

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import './index.css'; // Main CSS file, often for Tailwind CSS imports
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
